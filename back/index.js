bff2dc120da3363128f3eea8f9bab9413607d4c1
Ilyes Chrif <ilyeschrif@outlook.com>
2024-12-24 17:48:26 -0800

    Removed all files from the remote repository

--- back/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use("/",(req,res) => {
-    res.send("server is running !");
-})
-
-
-app.listen(5000,console.log('server is running on port 5000'));
